6c1342d462dd34e722541f8377436176563d6da6
maiki0419 <matsudamaiki@gmail.com>
2022-03-15 12:41:03 +0000

    チャット機能

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,11 +3,24 @@ class ChatsController < ApplicationController
 
 
   def create
-
+    @chat = current_customer.chats.new(chat_params)
+    if @chat.save
+      @chats = @chat.room.chats
+    else
+      @chats = @chat.room.chats
+    end
   end
 
   def destroy
+    @chat = Chat.find(params[:id])
+    @chats = @chat.room.chats
+    @chat.destroy
+  end
 
+  private
+
+  def chat_params
+    params.require(:chat).permit(:message, :room_id)
   end
 
 end
