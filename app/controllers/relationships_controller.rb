d58e8e82774bc004361322f2338bdeebfc9e2fd3
maiki0419 <matsudamaiki@gmail.com>
2022-03-18 08:27:25 +0000

    フォロー機能非同期

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,15 @@
 class RelationshipsController < ApplicationController
-before_action :authenticate_customer!
+
   def create
     current_customer.relationships.create(followed_id: params[:customer_id])
     # フォロー通知
     @customer = Customer.find(params[:customer_id])
     @customer.create_notification_follow!(current_customer)
-
-    redirect_to request.referer
   end
 
   def destroy
+    @customer = Customer.find(params[:customer_id])
     current_customer.relationships.find_by(followed_id: params[:customer_id]).destroy
-    redirect_to request.referer
   end
 
 
