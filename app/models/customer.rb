a824e12c20246f4d8026ae4b9c044e343a510f58
maiki0419 <matsudamaiki@gmail.com>
2022-03-22 12:44:26 +0000

    customerモデルテスト

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -18,7 +18,7 @@ class Customer < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
+# 通知機能アソシエーション
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
