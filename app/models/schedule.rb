2ea393ac15c996ed6b4672ce986a848108b8e174
maiki0419 <matsudamaiki@gmail.com>
2022-04-27 08:16:48 +0000

    バリデーション、コメントチャット日時、成績詳細戻るボタン追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,6 +2,13 @@ class Schedule < ApplicationRecord
 
   validates :team_id, presence: true
   validates :title, presence: true, length: {maximum: 20}
-
+  validate :start_end
   belongs_to :team
+
+  def start_end
+    if starts_at > ends_at
+      errors.add(:starts_at, "予定終了は予定開始より後の日時を入力してくだい。")
+    end
+  end
+
 end
